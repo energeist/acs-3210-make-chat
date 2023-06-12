23beda9d2eed0c81d863cf77e9d5f3ef1b46ce69
Mark Rattle <mark.rattle@gmail.com>
2023-06-12 18:35:41 -0400

    Login with a cool username

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const app = express();
 //Socket.io has to use the http server
 const server = require('http').Server(app);
 
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  // This file will be read on new socket connections
+  require('./sockets/chat.js')(io, socket);
+  console.log("🔌 New user connected! 🔌");
+})
+
 //Express View Engine for Handlebars
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.use('/public', express.static('public'));
+
+//Establish your public folder
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
